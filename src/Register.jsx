33eff9265b1f67b044950e477abc68513013b875
syedharishafeez <syed_haris98@yahoo.com>
2020-10-10 20:22:58 +0500

    Fetching Roles and also select box in roles

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import { useHistory } from "react-router-dom";
 import Paper from "@material-ui/core/Paper";
-import Select from '@material-ui/core/Select';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
+import Select from "@material-ui/core/Select";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
@@ -18,7 +19,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import url from "./urls";
 import { useSnackbar } from "notistack";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import {Link as RouterLink} from 'react-router-dom'
+import { Link as RouterLink } from "react-router-dom";
 
 function Copyright() {
   return (
@@ -39,7 +40,7 @@ const useStyles = makeStyles((theme) => ({
   },
   formControl: {
     // margin: theme.spacing(1),
-    width:"100%"
+    width: "100%",
   },
   image: {
     backgroundImage:
@@ -74,7 +75,8 @@ const useStyles = makeStyles((theme) => ({
 export default function Login() {
   const classes = useStyles();
   const history = useHistory();
-  const [roleId, setRoleId] = React.useState("");
+  const [roleIds, setRoleIds] = React.useState([]);
+  const [roles, setRoles] = React.useState([]);
   const [age, setAge] = React.useState("");
   const [gender, setGender] = React.useState("");
   const [department, setDepartment] = React.useState("");
@@ -84,27 +86,61 @@ export default function Login() {
 
   const { enqueueSnackbar } = useSnackbar();
 
-  function handleChangeDept(event){
-    setDepartment(event.target.value)
+  function handleChangeDept(event) {
+    setDepartment(event.target.value);
   }
 
-  function handleChangeGender(event){
-    setGender(event.target.value)
+  function handleChangeGender(event) {
+    setGender(event.target.value);
   }
 
+  function handleRoleChange(event) {
+    console.log(event.target);
+    event.persist();
+    setRoleIds(event.target.value);
+  }
+
+  useEffect(() => {
+    return (async function () {
+      let currentDataJSON = await fetch(url.FETCH_ROLES, {
+        method: "get",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      let currentData = await currentDataJSON.json();
+
+      setRoles(currentData.body.data ? currentData.body.data : []);
+    })();
+  }, []);
 
   async function handleRegistration(e) {
     e.preventDefault();
     setLoading(true);
     try {
-      if (userName && userPassword && roleId && age && department && gender) {
+      if (
+        userName &&
+        userPassword &&
+        roleIds.length &&
+        age &&
+        department &&
+        gender
+      ) {
         let currentDataJSON = await fetch(url.REGISTER, {
           method: "POST",
           headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ userName, userPassword, roleId, age, department, gender }),
+          body: JSON.stringify({
+            userName,
+            userPassword,
+            roleIds,
+            age,
+            department,
+            gender,
+          }),
         });
         let currentData = await currentDataJSON.json();
         if (currentData.statusCode === 200) {
@@ -122,6 +158,8 @@ export default function Login() {
     setLoading(false);
   }
 
+  console.log("roles = ", roles);
+
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
@@ -135,132 +173,128 @@ export default function Login() {
             Register
           </Typography>
           <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="name"
-              label="Username"
-              onInput={(e) => setUserName(e.target.value)}
-              autoFocus
-              required={true}
-            />
-            </Grid>
-            <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="Password"
-              type="password"
-              id="password"
-              onInput={(e) => setUserPassword(e.target.value)}
-              required={true}
-            />
-            </Grid>
-            
-            <Grid item xs={12} sm={6}>
-              <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="roleId"
-              label="Role ID"
-              onInput={(e) => setRoleId(e.target.value)}
-              autoFocus
-              required={true}
-            />
-              
-            </Grid>
-            <Grid item xs={12} sm={6}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="age"
-              label="Age"
-              type="Number"
-              onInput={(e) => setAge(e.target.value)}
-              autoFocus
-              required={true}
-            />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-            <FormControl variant="outlined" className={classes.formControl}>
-              <InputLabel htmlFor="outlined-age-native-simple">Department *</InputLabel>
-
-                <Select
-                  native
-                  value={department}
-                  onChange={handleChangeDept}
-                  label="Department *"
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="name"
+                  label="Username"
+                  onInput={(e) => setUserName(e.target.value)}
+                  autoFocus
                   required={true}
-                >
-                  <option value=""/>
-                  
-                  <option value="department1">
-                    Department 1
-                  </option>
-                  <option value="department2">
-                    Department 2
-                  </option>
-                  <option value="department3">
-                    Department 3
-                  </option>
-                  <option value="department4">
-                    Department 4
-                  </option>
-              </Select>
-            </FormControl>
-            
-              
-              
-            </Grid>
-            <Grid item xs={12} sm={6}>
-            <FormControl variant="outlined" className={classes.formControl}>
-            <InputLabel htmlFor="outlined-age-native-simple">Gender *</InputLabel>
-
-                <Select
-                  native
-                  value={gender}
-                  onChange={handleChangeGender}
-                  label="Gender *"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  id="password"
+                  onInput={(e) => setUserPassword(e.target.value)}
                   required={true}
-                >
-                  <option value="">
-                  </option>
-                  <option value="male">
-                    Male
-                  </option>
-                  <option value="female">
-                    Female
-                  </option>
-              </Select>
-            </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={handleRegistration}
+                />
+              </Grid>
 
-          >
-            {loading ? (
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  select
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="roleIds"
+                  label="Role ID"
+                  variant="outlined"
+                  SelectProps={{
+                    multiple: true,
+                    value: roleIds,
+                    onChange: handleRoleChange,
+                  }}
+                >
+                  {roles.map((item) => (
+                    <MenuItem value={item.ROLE_ID}>{item.ROLE_NAME}</MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="age"
+                  label="Age"
+                  type="Number"
+                  onInput={(e) => setAge(e.target.value)}
+                  autoFocus
+                  required={true}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <FormControl variant="outlined" className={classes.formControl}>
+                  <InputLabel htmlFor="outlined-age-native-simple">
+                    Department *
+                  </InputLabel>
+
+                  <Select
+                    native
+                    value={department}
+                    onChange={handleChangeDept}
+                    label="Department *"
+                    required={true}
+                  >
+                    <option value="" />
+
+                    <option value="department1">Department 1</option>
+                    <option value="department2">Department 2</option>
+                    <option value="department3">Department 3</option>
+                    <option value="department4">Department 4</option>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <FormControl variant="outlined" className={classes.formControl}>
+                  <InputLabel htmlFor="outlined-age-native-simple">
+                    Gender *
+                  </InputLabel>
+
+                  <Select
+                    native
+                    value={gender}
+                    onChange={handleChangeGender}
+                    label="Gender *"
+                    required={true}
+                  >
+                    <option value=""></option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleRegistration}
+            >
+              {loading ? (
                 <CircularProgress
                   size={24}
                   style={{
@@ -272,16 +306,16 @@ export default function Login() {
                 />
               ) : null}
               {"  "}Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <RouterLink to="/" variant="body2">
-                Already have an account? Sign in
-              </RouterLink>
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <RouterLink to="/" variant="body2">
+                  Already have an account? Sign in
+                </RouterLink>
+              </Grid>
             </Grid>
-          </Grid>
-        </form>
-          </div>
+          </form>
+        </div>
       </Grid>
     </Grid>
   );
--- src/urls.js
+++ src/urls.js
@@ -1,6 +1,6 @@
 export default {
-    LOGIN: "http://localhost:4000/login",
-    REGISTER: "http://localhost:4000/register",
-    DETAIL:"http://localhost:4000/employee-detail"
-  };
-  
+  LOGIN: "http://localhost:4000/login",
+  REGISTER: "http://localhost:4000/register",
+  DETAIL: "http://localhost:4000/employee-detail",
+  FETCH_ROLES: "http://localhost:4000/fetch-roles",
+};
